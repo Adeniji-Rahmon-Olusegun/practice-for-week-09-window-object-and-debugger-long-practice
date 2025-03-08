554d20e134ddcfda7b569ee29c78d849ca8065db
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2025-03-08 16:07:05 +0100

    manipualtion of DOM using window object

--- assets/js/manipulate.js
+++ assets/js/manipulate.js
@@ -1,18 +1,22 @@
-function changeTitle() {
+export function changeTitle() {
     // Change the title of the page to "(Your name)'s Portfolio"
 
     // Your code here
+    return window.document.title = "Rahmon";
 }
 
-function changeHeader() {
+export function changeHeader() {
     // Change the name in the h1 of the page to your name
 
     // Your code here
+    return window.document.body.children[0].children[0].innerText = "Rahmon Adeniji";
+    
 }
 
-function changeAboutMe() {
+export function changeAboutMe() {
     /* Update the first paragraph in the About Me section with a small
      passage about yourself */
 
     // Your code here
+    return window.document.body.children[1].children[1].innerText = "I love software Engineering";
 }
